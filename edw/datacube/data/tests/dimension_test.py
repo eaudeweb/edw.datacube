eaudeweb/edw.datacube

commit e85f710e01087a1117197f80a83fb501cea1ca70

Sparql queries optimizations for dimensions

edw/datacube/data/cube.py
@@ -341,6 +341,12 @@ class Cube(object):
     def get_dimension_options_n(self, dimension, filters, n_filters):
         common_uris = None
         result_sets = []
+        intervals = []
+        merged_intervals = []
+        uri_list = None
+        distinct_types = False
+        comparator = None
+
         for extra_filters in n_filters:
             query = sparql_env.get_template('dimension_options.sparql').render(**{
                 'dataset': self.dataset,
@@ -350,27 +356,48 @@ class Cube(object):
                 'notations': self.notations,
             })
             result_sets.append(list(self._execute(query)))
-        interval_types = set(item.get('interval_type')
-                             for res in result_sets for item in res)
 
-        if len(interval_types) > 1:
-            # normalize to years when time-period is expressed as different intervals
-            def options(res):
-                return set(item.get('parent_year') or item['uri'] for item in res)
+        def options(res):
+            return set(item['uri'] for item in res)
 
-        else:
-            def options(res):
-                return set(item['uri'] for item in res)
+        def get_interval_type(item):
+            uri = item.get('uri')
+            return uri.split('/')[-2]
 
+        # Make an uri list containing all uris and a common uri list containing
+        # uris common for all result sets
         for res in result_sets:
+            if not comparator:
+                if res:
+                    comparator = get_interval_type(res[0])
+            else:
+                for elem in res:
+                    if comparator != get_interval_type(elem):
+                        distinct_types = True
             res = options(res)
-            if common_uris is None:
+            if uri_list is None:
+                uri_list = res
                 common_uris = res
             else:
+                uri_list = uri_list | res
                 common_uris = common_uris & res
+
+        if dimension == 'time-period' and distinct_types:
+            # Query the intervals
+            query_intervals = sparql_env.get_template('dimension_options_intervals.sparql').render(**{
+                'uri_list': uri_list,
+            })
+            intervals.append(list(self._execute(query_intervals)))
+
+            merged_intervals = [item for interval in intervals
+                                for item in interval]
+            common_uris = set(item.get('parent_year')
+                              for item in merged_intervals)
+
         data = []
         for uri in common_uris:
             data.append((uri, dimension))
+
         # duplicates - e.g. when a breakdown is member of several breakdown groups
         labels1 = self.get_labels_with_duplicates(data)
         if labels1:

edw/datacube/browser/tests/ajax_test.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from mock import Mock, MagicMock, call, patch
 import simplejson as json
 import pytest

edw/datacube/data/templates/dimension_options.sparql
@@ -6,7 +6,7 @@ PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
 PREFIX dad-prop: <http://semantic.digital-agenda-data.eu/def/property/>
 PREFIX interval: <http://reference.data.gov.uk/def/intervals/>
 PREFIX time: <http://www.w3.org/2006/time#>
-SELECT DISTINCT ?uri {% if dimension_code == 'time-period' %} ?interval_type ?parent_year {% endif %} WHERE {
+SELECT DISTINCT ?uri WHERE {
 
 {%- set variable = '?option'
     if dimension_code in group_dimensions
@@ -28,18 +28,6 @@ SELECT DISTINCT ?uri {% if dimension_code == 'time-period' %} ?interval_type ?pa
     {%- set dimension_uri = notations.lookup_dimension_uri(dimension_code)['uri'] %}
     ?observation {{ dimension_uri|uri_n3 }} ?uri.
     ?observation {{ obs_uri|uri_n3 }} ?value .
-{% endif %}
-
-{% if dimension_code == 'time-period' %}
-    OPTIONAL {
-        ?uri a ?interval_type .
-        ?interval_type rdfs:subClassOf interval:CalendarInterval .
-    }
-
-    OPTIONAL {
-        ?uri time:intervalDuring ?parent_year .
-        ?parent_year a interval:Year .
-    }
 {% endif %}
 
 }

edw/datacube/data/templates/dimension_options_intervals.sparql
@@ -16,6 +16,6 @@ FILTER (
   {%- if not loop.last %} || {% endif %}
 {%- endfor %}
 )
-
 }
+
 LIMIT 1000

commit 82fde04610375f3aeeb45b13b5f12e644b55853e

Fixed a bug introduced in e85f710

edw/datacube/data/cube.py
@@ -387,7 +387,6 @@ class Cube(object):
             else:
                 uri_list = uri_list | res
                 common_uris = common_uris & res
-
         if dimension == 'time-period' and distinct_types:
             # Query the intervals
             query_intervals = sparql_env.get_template('dimension_options_intervals.sparql').render(**{
@@ -397,9 +396,24 @@ class Cube(object):
 
             merged_intervals = [item for interval in intervals
                                 for item in interval]
-            common_uris = set(item.get('parent_year')
-                              for item in merged_intervals)
 
+            # re-calculate common_uris based on parent interval
+            common_uris = None
+            for res in result_sets:
+                # get years from merged_intervals
+                years = []
+                for option in options(res):
+                  # search in merged_intervals
+                  interval = filter(lambda interval:interval['uri'] == option, merged_intervals)
+                  if ( interval == [] ):
+                      # this was a year, not present in merged_intervals
+                      years.append(option)
+                  else:
+                      years.append(interval[0]['parent_year'])
+                if common_uris is None:
+                    common_uris = set(years)
+                else:
+                    common_uris = common_uris & set(years)
         data = []
         for uri in common_uris:
             data.append((uri, dimension))

edw/datacube/data/tests/dimension_test.py
@@ -262,7 +262,7 @@ def test_indicator_groups_are_sorted():
     res = cube.get_dimension_options(dimension='indicator-group')
     codes = [y['notation'] for y in res]
     assert codes == ['telecom', 'broadband', 'bbquality', 'mobile',
-        'internet-usage', 'internet-services', 'egovernment', 'ecommerce',
+        'internet-usage', 'audiovisual', 'internet-services', 'egovernment', 'ecommerce',
         'ebusiness', 'ict-skills', 'ict-edu', 'eHealth',
         'research-and-development', 'ict-sector', 'back']
 
@@ -328,3 +328,43 @@ def test_dimension_options_xy_none_common():
     );
     # should return no result
     assert res == {}
+
+@sparql_test
+def test_dimension_options_xy_time_periods():
+    cube = create_cube()
+
+    res = cube.get_dimension_options_xy('time-period', [],
+         [('indicator', 'bb_hhi'),
+          ('breakdown', 'TOTAL_FBB'),
+          ('unit-measure', 'hhi')
+         ],
+         [
+          ('indicator', 'bb_scov'),
+          ('breakdown', 'TOTAL_POPHH'),
+          ('unit-measure', 'pc_hh_all')
+         ]
+    );
+    # should return only 2013
+    # the intersection of [2013-06,2014-06] and [2011,2012,2013]
+    codes = [y['notation'] for y in res]
+    assert codes == ['2013']
+
+@sparql_test
+def test_dimension_options_xy_time_periods2():
+    cube = create_cube()
+
+    res = cube.get_dimension_options_xy('time-period', [],
+         [('indicator', 'bb_hhi'),
+          ('breakdown', 'TOTAL_FBB'),
+          ('unit-measure', 'hhi')
+         ],
+         [
+          ('indicator', 'AAAA_cov'),
+          ('breakdown', '1M_websites'),
+          ('unit-measure', 'pc_websites')
+         ]
+    );
+    # should return only 2013
+    # the intersection of [2013-06,2014-06] and [2012-Q2, 2012-Q3, 2013-Q1..Q4]
+    codes = [y['notation'] for y in res]
+    assert codes == ['2013']